10ee62f74c9a0773391faa3e77b44b9af08e091f
Botond Balogh <balogh.botond8@gmail.com>
2024-08-21 16:13:55 +0200

    other extra toolbars

--- src/features/CKEditor/components/Editor.js
+++ src/features/CKEditor/components/Editor.js
@@ -320,6 +320,100 @@ const Editor = ({onChange}) => {
             columns: 12,
             colors: REDUCED_MATERIAL_COLORS,
           },
+          heading: {
+            options: [
+              {model: "paragraph", title: "Paragraph", class: "ck-heading_paragraph"},
+              {
+                model: "heading1",
+                view: "h1",
+                title: "Heading 1",
+                class: "ck-heading_heading1",
+              },
+              {
+                model: "heading2",
+                view: "h2",
+                title: "Heading 2",
+                class: "ck-heading_heading2",
+              },
+              {
+                model: "heading3",
+                view: "h3",
+                title: "Heading 3",
+                class: "ck-heading_heading3",
+              },
+              {
+                model: "heading4",
+                view: "h4",
+                title: "Heading 4",
+                class: "ck-heading_heading4",
+              },
+              {
+                model: "heading5",
+                view: "h5",
+                title: "Heading 5",
+                class: "ck-heading_heading5",
+              },
+              {
+                model: "heading6",
+                view: "h6",
+                title: "Heading 6",
+                class: "ck-heading_heading6",
+              },
+            ],
+          },
+          htmlSupport: {
+            allow: [
+              // Enables all HTML features.
+              {
+                name: /.*/,
+                attributes: true,
+                classes: true,
+                styles: true,
+              },
+            ],
+            disallow: [
+              {
+                attributes: [
+                  {key: /^on(.*)/i, value: true},
+                  {
+                    key: /.*/,
+                    value: /(\b)(on\S+)(\s*)=|javascript:|(<\s*)(\/*)script/i,
+                  },
+                  {key: /.*/, value: /data:(?!image\/(png|jpeg|gif|webp))/i},
+                ],
+              },
+              {name: "script"},
+            ],
+          },
+          image: {
+            resizeOptions: [
+              {
+                name: "resizeImage:original",
+                label: "Default image width",
+                value: null,
+              },
+              {
+                name: "resizeImage:50",
+                label: "50% page width",
+                value: "50",
+              },
+              {
+                name: "resizeImage:75",
+                label: "75% page width",
+                value: "75",
+              },
+            ],
+            toolbar: [
+              "imageTextAlternative",
+              "toggleImageCaption",
+              "|",
+              "imageStyle:inline",
+              "imageStyle:wrapText",
+              "imageStyle:breakText",
+              "|",
+              "resizeImage",
+            ],
+          },
           plugins: [
             Alignment,
             Autoformat,
