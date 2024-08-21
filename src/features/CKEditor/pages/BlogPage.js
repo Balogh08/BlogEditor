29d909c8145506612a02787b4d10de20269ee306
Botond Balogh <balogh.botond8@gmail.com>
2024-08-21 16:59:54 +0200

    preview edit

--- src/features/CKEditor/pages/BlogPage.js
+++ src/features/CKEditor/pages/BlogPage.js
@@ -3,18 +3,34 @@ import Editor from "../components/Editor";
 
 const EditorPage = () => {
   const [content, setContent] = useState("");
+  const [isEditing, setIsEditing] = useState(true);
+
+  const toggleMode = () => {
+    setIsEditing(!isEditing);
+  };
 
   return (
     <div>
       <h1>Blog Editor</h1>
-      <Editor onChange={setContent} />
-      <div style={{marginTop: "20px"}}>
-        <h2>Preview</h2>
-        <div
-          dangerouslySetInnerHTML={{__html: content}}
-          style={{border: "1px solid #ccc", padding: "10px"}}
-        />
-      </div>
+      {isEditing ? (
+        <div>
+          <Editor onChange={setContent} />
+          <button onClick={toggleMode} style={{marginTop: "20px"}}>
+            Preview
+          </button>
+        </div>
+      ) : (
+        <div>
+          <h2>Preview</h2>
+          <div
+            dangerouslySetInnerHTML={{__html: content}}
+            style={{border: "1px solid #ccc", padding: "10px"}}
+          />
+          <button onClick={toggleMode} style={{marginTop: "20px"}}>
+            Edit
+          </button>
+        </div>
+      )}
     </div>
   );
 };
