5f5bcea9cc93f64007cf50cbb04254d718267764
Botond Balogh <balogh.botond8@gmail.com>
2024-08-21 17:24:31 +0200

    Update BlogPage.js

--- src/features/CKEditor/pages/BlogPage.js
+++ src/features/CKEditor/pages/BlogPage.js
@@ -24,7 +24,12 @@ const EditorPage = () => {
           <h2>Preview</h2>
           <div
             dangerouslySetInnerHTML={{__html: content}}
-            style={{border: "1px solid #ccc", padding: "10px"}}
+            style={{border: "1px solid #ccc", padding: "10px",
+              maxHeight: "500px", // Set a maximum height to limit content height
+              maxWidth: "100%", // Set a maximum width relative to the parent container
+              overflow: "auto", // Ensure that scrollbars appear if the content exceeds the div's size
+              boxSizing: "border-box", // Ensure padding and border are included in the element's width and height
+            }}
           />
           <button onClick={toggleMode} style={{marginTop: "20px"}}>
             Edit
