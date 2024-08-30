a0d445aec3617acb2e4004498ce1572acc1c75fe
Botond Balogh <balogh.botond8@gmail.com>
2024-08-30 15:43:07 +0200

    Revert "DecoupledEditor style"
    This reverts commit 6822d3408c69fd4e796a02522678ce9dc3a33736.

--- src/features/CKEditor/components/Editor.js
+++ src/features/CKEditor/components/Editor.js
@@ -1,6 +1,6 @@
-import React, {useEffect, useState, useRef} from "react";
+import React, {useEffect, useState} from "react";
 import {CKEditor} from "@ckeditor/ckeditor5-react";
-import {DecoupledEditor} from "ckeditor5";
+import {ClassicEditor} from "ckeditor5";
 import {editorConfig} from "../config/editorConfig";
 
 import "ckeditor5/ckeditor5.css";
@@ -8,10 +8,6 @@ import "ckeditor5/ckeditor5.css";
 
 const Editor = ({onChange, initialData}) => {
   const [editorData, setEditorData] = useState("");
-  const editorContainerRef = useRef(null);
-  const editorMenuBarRef = useRef(null);
-  const editorToolbarRef = useRef(null);
-  const editorRef = useRef(null);
 
   useEffect(() => {
     setEditorData(initialData);
@@ -25,38 +21,12 @@ const Editor = ({onChange, initialData}) => {
 
   return (
     <div style={{width: "795px"}}>
-      <div>
-        <div className="main-container">
-          <div className="editor-container editor-container_document-editor" ref={editorContainerRef}>
-            <div className="editor-container__menu-bar" ref={editorMenuBarRef}></div>
-            <div className="editor-container__toolbar" ref={editorToolbarRef}></div>
-            <div className="editor-container__editor-wrapper">
-              <div className="editor-container__editor">
-                <div ref={editorRef}>
-                  <CKEditor
-                    onReady={(editor) => {
-                      editorToolbarRef.current.appendChild(
-                          editor.ui.view.toolbar.element);
-                      editorMenuBarRef.current.appendChild(
-                          editor.ui.view.menuBarView.element);
-                    }}
-                    onAfterDestroy={() => {
-                      Array.from(editorToolbarRef.current.children).forEach(
-                          (child) => child.remove());
-                      Array.from(editorMenuBarRef.current.children).forEach(
-                          (child) => child.remove());
-                    }}
-                    editor={DecoupledEditor}
-                    config={editorConfig}
-                    data={editorData}
-                    onChange={handleEditorChange}
-                  />
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      <CKEditor
+        editor={ClassicEditor}
+        config={editorConfig}
+        data={editorData}
+        onChange={handleEditorChange}
+      />
     </div>
   );
 };
